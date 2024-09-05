5709fbb55fe85e4e754f3e736f68a192ce5182fb
Sonatan Paul <iamsonatanpaul@gmail.com>
2024-09-05 22:45:18 +0600

    javascrpt recap task

---
+++ script.js
@@ -0,0 +1,62 @@
+// variable
+let name = "Sonatan Paul";
+
+// conditional
+let isGood = true;
+
+if (isGood) {
+  console.log("He is a good Man");
+} else {
+  console.log("He is a bad Man");
+}
+
+// Array
+const number = [3, 5, 6, 8, 4, 5, 5];
+console.log(number);
+let numberValue = number[2];
+console.log(numberValue);
+
+// Object
+
+const name = {
+  fullName: "Sonatan Paul",
+  age: 25,
+  isStudent: true,
+};
+console.log(student);
+
+const nameFull = name.fullName;
+const age = name["age"];
+console.log(nameFull);
+console.log(age);
+
+// function
+
+function result() {
+  console.log("hello world");
+}
+result();
+
+// parametar function
+
+function number(a, b) {
+  const result = a + b;
+  return result;
+}
+number(5, 6);
+
+// arrow function
+const arrowFunction = () => {
+  console.log("Hello World");
+};
+
+// loop
+for (let i = 0; (i) => 5; i++) {
+  console.log("i love you");
+}
+
+// spread operator
+const num = [2, 3, 6, 45, 6];
+console.log(num);
+const newNum = [...num];
+console.log(newNum);
